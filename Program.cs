3d19c07cf73d084e9225f8bc9e24b1e88b483fb1
mIINguta <gustavo.minguta@gmail.com>
2024-04-11 10:51:08 -0300

    Desafio Resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("21965718120", "Tijolão", "1112255", 32);
+Iphone iphone = new Iphone("2140028922", "15", "55588878", 128);
+
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Tigrinho");
+
+iphone.Ligar();
+iphone.InstalarAplicativo("FreeFire");
+
+
